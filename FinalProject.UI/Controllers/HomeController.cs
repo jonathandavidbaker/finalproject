c9ca602122945a7f5df478cc55fe9e5e41ba8e01
Jon Baker <jon.david.baker@gmail.com>
2019-04-29 09:52:07 -0500

    Added contact form

--- FinalProject.UI/Controllers/HomeController.cs
+++ FinalProject.UI/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 ﻿using FinalProject.DATA;
+using FinalProject.UI.Models;
 using Microsoft.AspNet.Identity;
+using System;
+using System.Net;
+using System.Net.Mail;
 using System.Web.Mvc;
 
 namespace IdentitySample.Controllers
@@ -31,6 +35,44 @@ namespace IdentitySample.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel contact)
+        {            
+            string body = $"Name: {contact.FirstName} {contact.LastName}<br>Email: {contact.Email}<br>Subject: {contact.Subject}<br>Message:<br>{contact.Message}";
+            
+            MailMessage msg = new MailMessage("no-reply@jdbaker.net",
+                                              "jon.david.baker@gmail.com",
+                                              "Email from Agency.com - " + contact.Subject,
+                                              body);
+            msg.IsBodyHtml = true;
+                        
+            SmtpClient client = new SmtpClient("mail.jdbaker.net");
+            client.Credentials = new NetworkCredential("no-reply@jdbaker.net", "pr$aM*Y*38V");
+            
+            using (client) 
+            {
+                try
+                {
+                    if (ModelState.IsValid)
+                    {
+                        client.Send(msg);
+                    }
+                    else
+                    {
+                        return View(contact);
+                    }
+                }
+                catch (Exception)
+                {
+                    ViewBag.ErrorMessage = "There was an error sending your message.  Please try again.";
+                    return View(contact);
+                }
+            }
+            
+            return View("ContactConfirmation", contact);
         }
     }
 }
